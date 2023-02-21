b84f5ac8a1fb98e86e9f1cc17fba8ced7df94e26
AlifSyahputra <alifsyahputra15@gmail.com>
2023-02-21 13:58:42 +0700

    [AS] feat// Memperbaiki Tampilan

--- portos/resources/views/editprofile.blade.php
+++ portos/resources/views/editprofile.blade.php
@@ -18,9 +18,8 @@
   <body>
     
     <x-navbar/>
-
       <!-- Background Edit Profile-->
-      <div class="d-flex " style="margin-top: 55px; margin-left: 235px;">
+      <div class="d-flex " style="margin-top: 55px; margin-left: 150px;">
         <div class="your-div">
           <img src="/background-edit-profile.jpg" alt="" style="width: 1240px; height: 365px; border-radius: 60px 0 0 0;">
         </div>
@@ -30,14 +29,14 @@
         </div>
 
         <div class="" style="margin-top: 380px; margin-left: 30px;">
-            <h4 class="fw-bold" style="font-family: poppins; font-size: 24px;">Edit Profile</h4>
-            <p class="fw-normal" style="font-family: poppins; font-size: 12px; margin-top: -5px;">Update your photo and personal details</p>
+            <h4 class="fw-bold" style="font-family: poppins; font-size: 24px;">Edit Profil</h4>
+            <p class="fw-normal" style="font-family: poppins; font-size: 12px; margin-top: -5px;">Lengkapi Foto dan Data Diri Anda</p>
         </div>
 
         <!-- Button Save dan Cancel -->
         <div class="text-end" style="margin-left: 535px; margin-top: 380px;">
-          <button class="btn mt-2 fw-bold rounded-4" style="font-family: poppins; color: #fff; background-color:#FFBE55; width: 120px; height: 40px;">Upload</button>
-          <button class="btn mt-2 fw-bold rounded-4 border border-3" style="font-family: poppins; color: black; background-color:#fff; width: 120px; height: 40px; margin-left: 10px;">Cancel</button>
+          <button class="btn mt-2 fw-bold rounded-4" style="font-family: poppins; color: #fff; background-color:#FFBE55; width: 120px; height: 40px;">Simpan</button>
+          <button class="btn mt-2 fw-bold rounded-4 border border-3" style="font-family: poppins; color: black; background-color:#fff; width: 120px; height: 40px; margin-left: 10px;">Batal</button>
         </div>
 
       </div>
@@ -45,20 +44,20 @@
       <!-- Nama-->
       <div class="d-flex  justify-content-center" style="margin-top: 100px;">
         <div class="col-auto" style="margin-left: -5px;">
-          <label for="inputPassword6" class="col-form-label fw-semibold" style="font-family: poppins; font-size: 20px;">Nama</label>
+          <label class="col-form-label fw-semibold" style="font-family: poppins; font-size: 20px;">Nama</label>
         </div>
         <div class="col-auto" style="margin-left: 105px;">
-          <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline" style="width: 800px; height: 40px;">
+          <input class="form-control" style="width: 800px; height: 40px;">
         </div>
       </div>
 
       <!-- Email-->
       <div class="d-flex  justify-content-center" style="margin-top: 50px;">
         <div class="col-auto" style="margin-left: -5px;">
-          <label for="inputPassword6" class="col-form-label fw-semibold" style="font-family: poppins; font-size: 20px;">Email</label>
+          <label class="col-form-label fw-semibold" style="font-family: poppins; font-size: 20px;">Email</label>
         </div>
         <div class="col-auto" style="margin-left: 105px;">
-          <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline" style="width: 800px; height: 40px;">
+          <input class="form-control" style="width: 800px; height: 40px;">
         </div>
       </div>
 
@@ -92,39 +91,11 @@
           <button class="btn btn-secondary dropdown-toggle text-dark" type="button" style="background-color: #fff;" id="dropdownMenuClickableOutside" data-bs-toggle="dropdown" data-bs-auto-close="inside" aria-expanded="false">
             Hari
           </button>
-          <ul class="dropdown-menu" aria-labelledby="dropdownMenuClickableOutside">
-            <li><a class="dropdown-item" href="#">1</a></li>
-            <li><a class="dropdown-item" href="#">2</a></li>
-            <li><a class="dropdown-item" href="#">3</a></li>
-            <li><a class="dropdown-item" href="#">4</a></li>
-            <li><a class="dropdown-item" href="#">5</a></li>
-            <li><a class="dropdown-item" href="#">6</a></li>
-            <li><a class="dropdown-item" href="#">7</a></li>
-            <li><a class="dropdown-item" href="#">8</a></li>
-            <li><a class="dropdown-item" href="#">9</a></li>
-            <li><a class="dropdown-item" href="#">10</a></li>
-            <li><a class="dropdown-item" href="#">11</a></li>
-            <li><a class="dropdown-item" href="#">11</a></li>
-            <li><a class="dropdown-item" href="#">12</a></li>
-            <li><a class="dropdown-item" href="#">13</a></li>
-            <li><a class="dropdown-item" href="#">14</a></li>
-            <li><a class="dropdown-item" href="#">15</a></li>
-            <li><a class="dropdown-item" href="#">16</a></li>
-            <li><a class="dropdown-item" href="#">17</a></li>
-            <li><a class="dropdown-item" href="#">18</a></li>
-            <li><a class="dropdown-item" href="#">19</a></li>
-            <li><a class="dropdown-item" href="#">20</a></li>
-            <li><a class="dropdown-item" href="#">21</a></li>
-            <li><a class="dropdown-item" href="#">22</a></li>
-            <li><a class="dropdown-item" href="#">23</a></li>
-            <li><a class="dropdown-item" href="#">24</a></li>
-            <li><a class="dropdown-item" href="#">25</a></li>
-            <li><a class="dropdown-item" href="#">26</a></li>
-            <li><a class="dropdown-item" href="#">27</a></li>
-            <li><a class="dropdown-item" href="#">28</a></li>
-            <li><a class="dropdown-item" href="#">29</a></li>
-            <li><a class="dropdown-item" href="#">30</a></li>
-            <li><a class="dropdown-item" href="#">31</a></li>
+          
+          <ul class="dropdown-menu" style="max-height: 300px; overflow-y: scroll;" aria-labelledby="dropdownMenuClickableOutside">  
+            @for ($i = 1; $i < 32 ;$i++)
+            <li><a class="dropdown-item" href="#">{{$i}}</a></li>  
+            @endfor
           </ul>
         </div>
 
@@ -133,7 +104,7 @@
           <button class="btn btn-secondary dropdown-toggle text-dark" type="button" style="background-color: #fff;" id="dropdownMenuClickableOutside" data-bs-toggle="dropdown" data-bs-auto-close="inside" aria-expanded="false">
             Bulan
           </button>
-          <ul class="dropdown-menu" aria-labelledby="dropdownMenuClickableOutside">
+          <ul class="dropdown-menu" style="max-height: 300px; overflow-y: scroll;" aria-labelledby="dropdownMenuClickableOutside">
             <li><a class="dropdown-item" href="#">Januari</a></li>
             <li><a class="dropdown-item" href="#">Februari</a></li>
             <li><a class="dropdown-item" href="#">Maret</a></li>
@@ -168,12 +139,13 @@
           <label class="col-form-label fw-semibold" style="font-family: poppins; font-size: 20px;">Bio</label>
         </div>
         <div class="col-auto" style="margin-left: 135px;">
-          <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline" style="width: 800px; height: 200px;">
+          <input class="form-control" style="width: 800px; height: 200px;">
         </div>
       </div>  
 
           <!-- Footer -->
-  <footer class="text-black" style="margin-top: 150px; background-color: #fff">
+          <x-footer/>
+  {{-- <footer class="text-black" style="margin-top: 150px; background-color: #fff">
     <!-- Grid container -->
     <div class="container p-4">
       <!--Grid row-->
@@ -258,7 +230,7 @@
       ©2022 PORTOS. All rights reserved
     </div>
     <!-- Copyright -->
-  </footer>
+  </footer> --}}
   <!-- Footer -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
--- portos/resources/views/explore.blade.php
+++ portos/resources/views/explore.blade.php
@@ -6,7 +6,26 @@
     <title>PORTOS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
+  
   <body>
+    $items = [
+    '1' => [
+        '1.1' => [
+            '1.1.1', '1.1.2'
+        ],
+        '1.2' => [
+            '1.2.1', '1.2.2'
+        ]
+    ],
+    '2' => [
+        '2.1' => [
+            '2.1.1', '2.1.2'
+        ],
+        '2.2' => [
+            '2.2.1', '2.2.2'
+        ]
+    ]
+];
     <x-navbar/>
     <div class="container" style="margin-left: -200px; margin-top: -30px;">
       <img src="/img-background-explore.svg" style="margin-left: 190px; width: 1518px; height: 400px;" alt="background-explore">
@@ -45,7 +64,18 @@
         <div class="row">
 
           <!-- Baris Pertama -->
-          <div class="col-md">
+          @foreach($items as $item)
+    @if($loop->first)
+        <p>Our first element of the array</p>
+    @endif
+
+    <p>{{ $loop->iteration . '/' . $loop->count }}</p>
+
+    @if($loop->last)
+        <p>Our last element of the array</p>
+    @endif
+@endforeach
+          {{-- <div class="col-md">
             <img src="/img-website-restaurant-1.svg" onclick="location.href='http://127.0.0.1:8000/detailporto'" alt="gambar-1" style="width: 300px; height: 200px;">
             <h5 class="fw-bold text-white mx-3">Restaurant - Mobile Apps</h5>
             <p class="text-white fw-semibold mx-3">Kazuha</p>
@@ -127,99 +157,14 @@
             <h5 class="fw-bold text-white mx-3">Restaurant - Mobile Apps</h5>
             <p class="text-white fw-semibold mx-3">Kazuha</p>
           </div>
-
+ --}}
 
         </div>
         
       </div>
 
           <!-- Footer -->
-  <footer class="text-black" style="margin-top: 150px; background-color: #fff">
-    <!-- Grid container -->
-    <div class="container p-4">
-      <!--Grid row-->
-      <div class="row">
-        <!--Grid column-->
-        <div class="col-lg-6 col-md-4 mb-4 mb-md-0 " style="margin-left: -130px;">
-          <h5 class="text-uppercase fw-bold" style="margin-left: -468px;">PORTOS</h5>
-          <p class="fw-semibold" style="color: #ADADAD;">Our vision is to provide convenience and help increase your sales business.</p>
-
-        </div>
-        <!--Grid column-->
-
-        <!--Grid column-->
-        <div class="col-lg-2 col-md-2 mb-4 mb-md-0">
-          <h5 class="fw-bold">Tentang</h5>
-
-          <ul class="list-unstyled mb-0">
-            <li>
-              <p class="fw-semibold text-secondary" style="margin-left: 20px;">How it works</p>
-            </li>
-            <li>
-              <p class="fw-semibold text-secondary" style="margin-left: -10px;" >Featured</p>
-            </li>
-            <li>
-              <p class="fw-semibold text-secondary" style="margin-left: 30px; width: 200px;">Partnership Business</p>
-            </li>
-            <li>
-              <p class="fw-semibold text-secondary" style="margin-left: -15px;">Relation</p>
-            </li>
-          </ul>
-        </div>
-        <!--Grid column-->
-
-        <!--Grid column-->
-        <div class="col-lg-2 col-md-2 mb-4 mb-md-0" style="margin-left: 40px;">
-          <h5 class="fw-bold">Komunitas</h5>
-
-          <ul class="list-unstyled mt-2">
-            <li>
-              <p class="fw-semibold text-secondary" style="margin-left: -50px;">Events</p>
-            </li>
-            <li>
-              <p class="fw-semibold text-secondary" style="margin-left: -65px;">Blog</p>
-            </li>
-            <li>
-              <p class="fw-semibold text-secondary" style="margin-left: -45px;">Podcast</p>
-            </li>
-            <li>
-              <p class="fw-semibold text-secondary" style="">Invite a friend</p>
-            </li>
-          </ul>
-        </div>
-        <!--Grid column-->
-
-        <!--Grid column-->
-        <div class="col-lg-2 col-md-2 mb-4 mb-md-0">
-          <h5 class="fw-bold">Sosial Media</h5>
-
-          <ul class="list-unstyled mt-2">
-            <li>
-              <p class="fw-semibold text-secondary" style="margin-left: -60px;">Discord</p>
-            </li>
-            <li>
-              <p class="fw-semibold text-secondary" style="margin-left: -40px;">Instagram</p>
-            </li>
-            <li>
-              <p class="fw-semibold text-secondary" style="margin-left: -60px;">Twitter</p>
-            </li>
-            <li>
-              <p class="fw-semibold text-secondary" style="margin-left: -40px;">Facebook</p>
-            </li>
-          </ul>
-        </div>
-        <!--Grid column-->
-      </div>
-      <!--Grid row-->
-    </div>
-    <!-- Grid container -->
-
-    <!-- Copyright -->
-    <div class="text-start p-3 fw-bold" style="background-color: #ffffff; margin-left: -100px;">
-      ©2022 PORTOS. All rights reserved
-    </div>
-    <!-- Copyright -->
-  </footer>
+          <x-footer/>
   <!-- Footer -->
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
