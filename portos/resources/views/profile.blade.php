4dc1c0df2c9ffb51cbd5a210074bfeffae939e7d
RasyidNafsyarie <rasyidnafsyarie@gmail.com>
2023-02-21 22:08:04 +0700

    [RN] fix:full width profile

--- portos/resources/views/profile.blade.php
+++ portos/resources/views/profile.blade.php
@@ -98,12 +98,12 @@
           solo show in Korea</p>
 
         <div class="container">
-          <div class="row row-cols-auto justify-content-center">  
+          <div class="row row-cols-auto justify-content-center">
             <button class="btn rounded-pill fw-semibold text-white" onclick="location.href='http://127.0.0.1:8000/editprofile'" style="background-color: #FFBE55; width: 70px; height: 30px; font-size: 8px;">Edit Profile</button>
             <img src="/icon-inbox.svg" style="width: 50px; height: 50px; margin-top: -10px; margin-left: 5px;" alt="Button-Inbox">
             <img src="/icon-logout.svg" onclick="location.href='http://127.0.0.1:8000/login'" style="width: 50px; height: 50px; margin-top: -10px; margin-left: -5px;" alt="Button-Logout">
           </div>
-        </div>  
+        </div>
 
         <div class="container">
           <div class="row row-cols-auto justify-content-center mt-4">
@@ -119,14 +119,14 @@
           <p class="fw-semibold" style="font-size: 10px; color: #00000030;">Member since Nov 15, 2022</p>
         </div>
 
-        </div>  
+        </div>
 
       </div>
-      
+
     </div>
 
   <x-footer/>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
